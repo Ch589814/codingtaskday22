397ff650ff8f147ad940050a8ead41e929e7ce74
Ch589814 <niyomufashadelphine1@gmail.com>
2026-02-18 13:34:00 +0200

    Add comprehensive test cases for mergeArrays method

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Arrays;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+
+        // Integer arrays
+        Integer[] intArray1 = {1, 2, 3};
+        Integer[] intArray2 = {4, 5, 6};
+
+        Integer[] mergedIntArray = GenericArrayUtility.mergeArrays(intArray1, intArray2);
+        System.out.println("Merged Integer Array: " + Arrays.toString(mergedIntArray));
+
+        // String arrays
+        String[] stringArray1 = {"orange", "lemon", "apple"};
+        String[] stringArray2 = {"cherry", "date"};
+
+        String[] mergedStringArray = GenericArrayUtility.mergeArrays(stringArray1, stringArray2);
+        System.out.println("Merged String Array: " + Arrays.toString(mergedStringArray));
+
+
+        //  First array is null
+        Integer[] nullArray = null;
+        Integer[] array = {10, 20, 30};
+        Integer[] result1 = GenericArrayUtility.mergeArrays(nullArray, array);
+        System.out.println("Merge null and [10, 20, 30]: " + Arrays.toString(result1));
+
+        // Second array is null
+        Integer[] array2 = {100, 200};
+        Integer[] result2 = GenericArrayUtility.mergeArrays(array2, nullArray);
+        System.out.println("Merge [100, 200] and null: " + Arrays.toString(result2));
+
+        //  Both arrays are null
+        Integer[] result3 = GenericArrayUtility.mergeArrays(nullArray, nullArray);
+        System.out.println("Merge null and null: " + Arrays.toString(result3));
+
+        //  Empty arrays
+        Integer[] emptyArray1 = {};
+        Integer[] emptyArray2 = {5, 6};
+        Integer[] result4 = GenericArrayUtility.mergeArrays(emptyArray1, emptyArray2);
+        System.out.println("merged empty and [5,6]:" + Arrays.toString(result4));
+    }
 }
